54ff751014289a5f21a2256953ce09ac17a5d622
Rafał Różak <rafalrozak@gmail.com>
2016-04-18 15:33:06 +0200

    share loggers between users

--- lib/social_traffic.ex
+++ lib/social_traffic.ex
@@ -1,4 +1,6 @@
 defmodule SocialTraffic do
+  @logger_cap 100
+
   def start(file) do
     {n, edges} = GraphReader.read(file)
               |> GraphReader.parse_input
@@ -7,17 +9,17 @@ defmodule SocialTraffic do
     user_refs
   end
 
-  def start_users(0, user_refs) do
+  def start_users(n, user_refs, logger_pid \\ nil)
+
+  def start_users(0, user_refs, _logger_pid) do
     user_refs
   end
 
-  def start_users(n, user_refs) do
-    {:ok, pid} = GenEvent.start_link([])
-    logfile = open_logfile n
-    GenEvent.add_handler(pid, LoggerHandler, {[], logfile})
-    {:ok, pid} = User.start_link(n - 1, [], pid)
-    IO.puts "#{n - 1}: #{inspect(pid)}"
-    start_users(n - 1, Map.put(user_refs, n - 1, pid))
+  def start_users(n, user_refs, logger_pid) do
+    logger_pid = start_logger(n, logger_pid)
+    {:ok, pid} = User.start_link(n - 1, [], logger_pid)
+    # IO.puts "#{n - 1}: #{inspect(pid)}"
+    start_users(n - 1, Map.put(user_refs, n - 1, pid), logger_pid)
   end
 
   def add_friendships(edges, user_refs) do
@@ -37,5 +39,20 @@ defmodule SocialTraffic do
     {:ok, logfile} = File.open log_name, [:write]
 
     logfile
+  end
+
+  defp start_logger(n, nil) do
+    {:ok, pid} = GenEvent.start_link([])
+    logfile = open_logfile n
+    GenEvent.add_handler(pid, LoggerHandler, {[], logfile})
+    pid
+  end
+
+  defp start_logger(n, pid) when rem(n, @logger_cap) == 0 do
+    start_logger(n, nil)
+  end
+
+  defp start_logger(n, pid) do
+    pid
   end
 end
